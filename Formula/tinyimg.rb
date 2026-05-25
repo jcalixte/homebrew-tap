9901d2dc49c042a4c73c38bb4bd0f526541b1865
Julien Calixte <julien.calixte@theodo.com>
2026-05-25 13:55:19 +0200

    tinyimg 1.3.0

--- Formula/tinyimg.rb
+++ Formula/tinyimg.rb
@@ -1,8 +1,8 @@
 class Tinyimg < Formula
   desc "Losslessly optimize PNG and JPG images with a minimal terminal UI"
   homepage "https://github.com/jcalixte/tinyimg"
-  url "https://github.com/jcalixte/tinyimg/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "889af923648ac99d3fb9612e15221fee5905934d8955482698e70f28d404abc4"
+  url "https://github.com/jcalixte/tinyimg/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "2f7db303f4c8060f39027ed2edb2545a03b557394806e3e0256aa6d89a088753"
   license "MIT"
 
   depends_on "gleam" => :build
@@ -21,6 +21,6 @@ class Tinyimg < Formula
   end
 
   test do
-    assert_match "1.2.0", shell_output("#{bin}/tinyimg --version")
+    assert_match "1.3.0", shell_output("#{bin}/tinyimg --version")
   end
 end
